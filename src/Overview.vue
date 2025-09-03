d6044bc18548306e3a0ec4ddc9c2e35809138a4c
Espen Strand Rørvik <espen.strand.rorvik@kristiansund.kommune.no>
2025-09-03 10:21:43 +0200

    wip add user info in overview page

--- src/Overview.vue
+++ src/Overview.vue
@@ -1,15 +1,40 @@
 <template>
   <div class="d-flex" style="align-items: center; justify-content: center;">
     <div>
-      <div class="bg-white d-flex align-items-center p-3 mb-3" style="border-radius: 8px;">
-        <img src="https://randomuser.me/api/portraits/men/8.jpg" alt="User photo" class="user-photo" style="width: 40px; height: 40px; border-radius: 32px;">
-        <div class="ms-2">
-          <p class="mb-0" style="color: black;"><strong>Philip Pedersen</strong></p>
-          <p class="mb-0" style="color: black;">19.03.2015</p>
+      <div class="bg-white p-3 mb-3" style="border-radius: 8px;">
+        <div class="d-flex mb-3">
+          <img src="https://randomuser.me/api/portraits/men/8.jpg" alt="User photo" class="user-photo" style="width: 80px; height: 80px; border-radius: 64px;">
+          <div>
+            <p class="ms-2 mb-2" style="color: black;"><strong>Philip Pedersen</strong></p>
+            <div class="ms-2 d-flex">
+              <div>
+                <p class="me-2 mb-0" style="color: black;"><strong>Født</strong></p>
+                <p class="me-2 mb-0" style="color: black;"><strong>Adresse</strong></p>
+                <p class="me-2 mb-0" style="color: black;"><strong>Sted</strong></p>
+              </div>
+              <div>
+                <p class="mb-0" style="color: black;">19.03.2015</p>
+                <p class="mb-0" style="color: black;">Gateadresse 01</p>
+                <p class="mb-0" style="color: black;">6500 Kristiansund</p>
+              </div>
+            </div>
+            <!-- <div class="ms-2 d-flex">
+              <p class="me-2 mb-0" style="color: black;"><strong>Født</strong></p>
+              <p class="mb-0" style="color: black;">19.03.2015</p>
+            </div>
+            <div class="ms-2 d-flex">
+              <p class="me-2 mb-0" style="color: black;"><strong>Adresse</strong></p>
+              <p class="mb-0" style="color: black;">Gateadresse 01</p>
+            </div>
+            <div class="ms-2 d-flex">
+              <p class="me-2 mb-0" style="color: black;"><strong>Sted</strong></p>
+              <p class="mb-0" style="color: black;">6500 Kristiansund</p>
+            </div> -->
+          </div>
         </div>
-      </div>
-      <div class="alert alert-info p-2" role="alert">
-        Du har ikke tilgang til å se alle opplysninger om brukeren. Ved behov, be om innsyn i den aktuelle saken.
+        <p class="mb-0" style="color: black;"><strong>Første aktivitet registrert 13.11.2023</strong></p>
+        <p class="mb-0" style="color: black;">Bruker er glad i xxxx, har noen utfordringer med xxxx. Bor til vanlig sammen med xxx, som har 80% omsorg.</p>
+        <p class="mb-0" style="color: black;">Beskrives av lærer som xxxxxx og xxxxxx. Trivsel på skolen oppsummeres som xxx.</p>
       </div>
       <AccordionCustom />
     </div>
